630868c7983b193ecff4a66ff77f93b869837a52
Shivali7Singh <shivalisingh168@gmail.com>
2026-01-23 12:46:25 +0530

    Add files via upload

---
+++ Transfer statements.py
@@ -0,0 +1,40 @@
+# Transfer statements
+# Break
+# for i in range(1,11):
+#     if  i ==6:
+#         break
+#     print(i)
+
+
+# Continue
+# for i in range(1,11):
+#     if i ==5:
+#         continue
+#     print(i)
+
+
+#Continue
+# for i in range(1,11):
+#      if i in [4,5]:
+#          continue
+#      print(i)
+
+
+# #Pass
+# for i in range(1,6):
+#     if i ==3:
+#         pass
+#     print(i)
+
+
+#Break and Continue
+# for i in range(1,11):
+#     if i ==8:
+#         break
+#     if i in [4,5]:
+#         continue
+#     print(i)
+
+
+
+
