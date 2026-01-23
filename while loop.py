e7e1a504e9cbfd4446a0cc47935d930c3d09c2bd
Shivali7Singh <shivalisingh168@gmail.com>
2026-01-23 12:45:19 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,187 @@
+# x = 2
+# while x > 0:
+#     x += 2
+#     print(x)
+
+# money = 0
+# while money < 1000:
+#     money += 50
+#     print("Added money:", money)
+# else: 
+#     print("Contribnution done RS. 1000 ")
+
+# import time
+# num= 5
+# while num > 0:
+#     print(num)
+#     num-=1
+#     time.sleep(1)
+# print("time's up")
+
+# Apple = 20
+# while Apple > 0:
+#     Apple -= 1
+#     print("Eat apple:", Apple)
+#     Apple -= 1
+# print("Finished!")
+
+# password = input("enter your password:")
+# while password != "shivali@123":
+#       print("incorrect password")
+#       password = input("enter your password:")
+# print("login successful")
+
+# import time
+# classname = 0
+# while classname <= 5:
+#     print("class:", classname)
+#     for i in range(1,11):
+#        print(i, end =" ")
+#     print("\n Attandance Done")
+#     time.sleep(3)
+#     classname += 1
+
+# classname = 0
+# while classname<=7:
+#     print("class:", classname)
+#     for i in range(1,11):
+#         print(i, end=" ")
+#         print("\n Attendance Done")
+# classname += 1
+
+# Table 1,2,3:
+# num = 2
+# while num <= 4:
+#     print(f"\n Multification table of {num}")
+#     for i in range(1,11):
+#         print(f"{num} x {i} = {num * i}")
+#     num += 1
+
+# import time
+# for round in range (1,4):
+#     print("Round:", round)
+#     count = 3
+#     while count > 0:
+#         print("contdown:", count)
+#         count -= 1
+#         time.sleep(2)
+#     print("Round completed")
+#     print("Next round")
+# Question 1-
+# for n in range(2,5):
+#     print("Table of" , n)
+#     for i in range(1,11):
+#        print(n, "x", i, "=", n*i)
+#     print("table complet")
+
+# # Queation 2-
+# import time 
+# round = 1
+# while round <= 3:
+#     print("round", round, end = " ")
+#     for i in range(round):
+#         print("*", end =" ")
+#         time.sleep(3)
+#     print()
+#     round += 1
+
+# #Question 3-
+# day = 1
+# while day <= 3:
+#     print("day:", day, end=" ")
+#     for i in("Morning", "Afternoon", "Evening"):
+#         print(i, end=" ")
+#     print()
+#     day += 1
+
+# user = 1
+# while user <=3:
+#     print("user:", user)
+#     balance = 1000
+#     for i in range(1,4):
+#         amount = int(input("enter your amount:"))
+#         if amount <= balance:
+#             balance = balance-amount
+#             print("remaning balance:", balance)
+#         else:
+#             print("insufficiant balance")
+#     print("user:" , user, " transancation completed")
+#     user += 1
+
+# user = 1
+# originalbalance = {1:1000, 2:5000,3:6000}
+# while user<=3:
+#     print("user:", user)
+#     balance=originalbalance[user]
+#     for i in range(1,4):
+#         amount=int(input("Enter your amount:"))
+#         if amount <= balance:
+#             balance=balance+amount
+#             print("remining balance:", balance)
+#         else:
+#             print("insufficiant balance")
+#     print("user:", user, " transancation completed")
+#     user += 1
+# Resturent Bill
+# customer = 1
+
+# while customer <= 4:
+#     print("customer:", customer)
+#     totel = 0
+#     for i in range(1,3):
+#         amount=int(input("enter your amount:"))
+#         totel=totel+amount
+#     print("totel bill:", totel)
+#     print("next customer")
+#     customer += 1
+        
+# student = 1
+# while student <=3:
+#     print("student:", student)
+#     total_marks = 0
+#     for subject in range(1,6):
+#         marks = int(input("enter marks for subject:"))
+#         total_marks += marks
+#         average = total_marks/5
+#     print("total marks:", total_marks)
+#     print("average:", average)
+# student += 1
+
+# Transfer Statements:
+
+# Break
+# for i in range(1,10):
+#     if i == 5:
+#         break
+#     print(i)
+
+
+# Continue
+# for i in range(1,11):
+#     if i ==3:
+#         continue
+#     if i ==5:
+#         continue
+#     if i ==7:
+#         continue
+#     print(i)
+
+
+# Pass
+# for i in range(1,11):
+#     pass
+#     for i in range(1,11):
+#         print(i)
+
+    
+        
+
+
+
+          
+
+
+
+
+
+      
