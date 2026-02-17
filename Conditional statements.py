c5f0cdb994a90080372530ca112695bfde64cbb4
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 15:12:37 +0530

    Add files via upload

---
+++ Conditional statements.py
@@ -0,0 +1,11 @@
+#Recharge Amount Validation
+# def recharge_amount(amount):
+#     amount = float(input("Enter recharge amount:"))
+#     if amount < 10:
+#         print("Reject")
+#     elif 10 <= amount <= 100:
+#         print("Valid")
+#     else:
+#      print("Choose proper pack")
+# amount = float(input("Enter recharge amount:"))
+# recharge_amount(amount)
