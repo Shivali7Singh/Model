c5065621640a9ddc5c19d320750fca83db7c7367
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 15:08:01 +0530

    Add files via upload

---
+++ File handling.py
@@ -0,0 +1,30 @@
+# NEW FOLDER BANANE KE LIYE
+import os
+# os.mkdir("shagun")
+#RENAME KRNE KE LIYE
+# os.rename("shagun","AIPA")
+#REMOVE DIRECTORY
+# os.rmdir("AIPA")
+
+# import shutil
+# shutil.rmtree("AIPA")
+#LIST ALL FILES IN FOLDER:
+# xyz=os.listdir(".")
+# print(xyz)
+# xyz=os.listdir()
+# print(xyz)(".")
+# abc=os.getcwd()
+# print(abc)
+
+# import os
+# os.mkdir("AIPA")
+# with open("AIPA/student.txt","w")as file:
+#     file.write("""students details: Anshika,Soni,Priya,Shipra""")
+# print("details add")
+
+#CREATE A FOLDER
+import os
+# os.mkdir("student data")
+# os.mkdir("student data/Assignment")
+#RENAME THE FOLDER:
+os.rename("student data/Assignments","student data/Submissions")
