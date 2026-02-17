cba5360329f9a412b35083d3d316772e3cf6d4f5
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 14:59:15 +0530

    Add files via upload

---
+++ python practice.py
@@ -0,0 +1,37 @@
+
+# print("The sum of 20 & 30 is:",20+30)
+# print("The sum of 30&10 is:")
+# print("The multification of 7 & 9 is:",7*9)
+# print("The different of 56&42 is:", 56-42)
+# print("The divison of 12&3 is:", 12/3)
+# print(f"sum of {5}+{3} is: {5+3}")
+# print(f"sum of 5+6 is: {5+6}")
+# print(f"The multification of 6&8 is: {6*8}")
+# print(f"The different of 20&32 is: {20-32}")
+# print(f"The divison of 17&8 is: {17/8}")
+# x=1
+# y=2
+# c=x+y
+# print(f"The sum of x&y is:{c}")
+# print("The sum of x&y is:", c)
+# a=6
+# b=8
+# c=a+b, 
+# d=a-b
+# e=a*b
+# f=a/b
+# print("The sum of",a,"and",b,"is",c)
+# print("The different of",a,"and",b,"is",d)
+# print("The multifiction of",a,"and",b,"is",e)
+# print("The divison of",a,"and",b,"is",f)
+# print(f'sum of a,b, is {a+b}')
+# print(f"sum of a,b is:",c)
+# print(f'sum of{a},{b},is:{c}')
+print("The sum of 40 & 80 is:", 40+80)
+print(f"the subtraction of {5}+{9} is: {5+9}")
+print(f"the modulse of {3}%{6} is: {3%6}")
+x = 9
+y = 20
+z = x + y
+print(f"the sum of x&y is: {z}")
+print(f"the ")
