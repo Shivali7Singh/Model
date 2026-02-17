86bc7b48de04247cff5750396df56caf3ef76deb
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 15:06:30 +0530

    Add files via upload

---
+++ loop concept.py
@@ -0,0 +1,81 @@
+# name = "shivali singh"
+# for i in range(21):
+#     print(name ,str(i))
+
+# number = 8
+# for i in range (3,10,2):
+#     print(number, "X", i, "=", number*i)
+# else:
+#     print("table completetd")
+
+# for i in "shivali singh":
+#     print(i)
+
+
+# # Using For Loop:
+# for i in range(1,11):
+#     print(i)
+
+# for m in range(2,21):
+#     print(m)
+
+# Using For Loop:
+name = "shivali"
+for i in range(5):
+    print(name)
+x="Hello World"
+for y in range(101):
+    print(x)
+a="shivali singh"
+for b in range(102):
+    print(a)
+
+
+
+
+# Even Number:
+# for i in range(2,21,2):
+#     print(i)
+
+# Odd Number:
+# for i in range(1,20,2):
+#     print(i)
+
+# Square:
+# number = int(input("enter the number:"))
+# for i in range(1,number+1):
+#     sq = i*i
+#     print(sq)
+
+
+# Reverse Order:
+# for i in range(10,0,-1):    
+#  print(i)
+
+# number = int(input("enter the number:"))
+# for i in range(1,12,2):
+#     print(number, "is multiple by",i, "=", number*i)
+
+# Sum of 10 Natural Numbers:
+# total = sum(range(1,11))
+# print(total)
+
+
+#Factorical Number:
+# number = int(input("enter the factorial number:"))
+# factorial = 1
+# for i in range(1,number+1):
+#     factorial = factorial*i
+#     print("factorial of", number, "is",factorial)
+
+# # Print each Charactor of the Word:
+# for i in "Python":
+#     print(i)
+
+#Second method
+# food =input("food: ")
+# eat = "yes" if food == "cake" else "no"
+# print(eat)
+
+# food = input("food: ")
+# print("sweet") if food == "cake" or food =="jalebi" else print("not sweet")     
