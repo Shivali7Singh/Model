3a60f8a23250ecc85badbca6b2211a3cc7efc569
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 14:57:07 +0530

    Add files via upload

---
+++ SSR.py
@@ -0,0 +1,21 @@
+# print("Hello world")
+# print("shipra") #in-line print
+# print("shipra \n yadav") #next line print
+# print("""SDSFCDSC abfhbnmkg sjkrm,mx nckdc afghhhjfgn;jfhfjgmn;dasf ahfgldlknhjg;iyihhj""") #multi line print
+# firstname = "shivali " 
+# lastname = "singh" 
+# print("My name is " + firstname + lastname)
+
+# x = "shivali "
+# y = "singh"
+# print("My name is " + x + y)
+
+# age = 18
+# print(f"I am {age} years old. ")
+
+# print("Shivali, ", end="")
+# print("Singh!")
+# print("This is a multi-line print using escape characters. \n\tLine 1\n\tLine 2\n\tLine 3")
+# print("The answer is", 42, "and pi is approximately", 3.14) 
+print("""This is an example of multi-line text.\n It is written over several lines,\n and each line will be printed exactly as it appeears here. """)
+# print("Hello Everyon")
