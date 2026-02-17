c90f444673601a8e6ec1a6502a41bbd416a967a3
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 15:05:43 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,56 @@
+# class student:
+#   def __init__(self,name,trade):
+#          self.name=name
+#          self.trade=trade
+# S1=student("Shivali","AIPA")
+# S2=student("Soni","COPA")
+# print(S1.name,S1.trade)
+# print(S2.name,S2.trade)
+
+# class song:
+#      def __init__(self,name,singer,writter):
+#           self.name=name
+#           self.singer=singer
+#           self.writter=writter
+# x1=song("Ranjhana","Arijit Singh","Irshad kamil")
+# x2=song("Mahi ve","Neha kakkar","Javed akhtar")
+# print(x1.name,x1.singer,x1.writter)
+# print(x2.name,x2.singer,x2.writter)
+
+# class BankAccount:
+#     def __init__(self,balance):
+#         self.balance=balance
+#     def  add(self,amt):
+#         self.balance+=amt
+#     def withdraw(self,amt):
+#         self.balance-=amt
+#     def finalAmount(self):
+#       return self.balance
+# acc=BankAccount(100)
+# acc.add(50)
+# acc.withdraw(100)
+# print(acc.finalAmount())
+
+# class BankAccount:
+#     def __init__(self,balance):
+#         self.balance=balance
+#     def add(self,amt):
+#             self.balance+=amt
+#     def withdraw(self,amt):
+#             self.balance-=amt
+#     def finalAmount(self):
+#      return self.balance
+# initial =int(input("Enter the Amount"))
+# deposit=int(input("Enter the Deposit"))
+# acc=BankAccount(initial)
+# acc.add(deposit)
+# acc.withdraw(600)
+# print(acc.finalAmount())
+
+print("Hello,world!",end="")
+print("This is a single line.")
+    
+        
+        
+         
+
