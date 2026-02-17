96cebea6b412dc15abfe0baccbfa370c638048c3
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 14:57:34 +0530

    Add files via upload

---
+++ SS.py
@@ -0,0 +1,23 @@
+# print("Hello world")
+# print("shipra shivali")
+# print("Hello world")
+
+# print("shailesh") #in-line print 
+
+
+# print("shailesh \n yadav") #next line print
+
+print("""SDSFCDSC absbdkxcjanjcn sjkndx,mx nckdc \n adclkjidlsamjl;kmcx;dasc \n alkdjnclkjasm;oxlkcds;c
+
+""") #multi line
+# print("Hello world")
+# print("shivali") #in-line print
+# print("shivali\n singh") #next line print
+
+
+# def cube(x):
+#     return x**3
+# def add(a,b):
+#     return a+b
+# def hello():
+#     print("welcome nsti")
