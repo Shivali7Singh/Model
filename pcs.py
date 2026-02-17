9cc24f2d27a41dea3624f8a1caadefdcca9eb6a3
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 15:05:09 +0530

    Add files via upload

---
+++ pcs.py
@@ -0,0 +1,193 @@
+
+# Student grading system:
+
+# marks = int(input("out of 100:"))
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >= 75:
+#     print("Grade B") 
+# elif marks >= 50:
+#     print("Grade C") 
+# else:
+#     print("fail")     
+
+# Temperature checker
+
+# temperature = int(input("current temperature"))
+# if temperature > 30:
+#     print("it's a hot day!")
+# elif 20 <= temperature <= 30:
+#     print("nice weather!")
+# else:
+#     print("it's cold today")   
+
+# Traffic single decision:
+
+# signal_colors = input("signal color:")
+# if signal_colors == ("red"):
+#     print("stop")
+# elif signal_colors == ("yellow"): 
+#     print("get ready")
+# else:
+#    print("go")
+
+#Even or odd number:
+
+# number = int(input("enter number:"))
+# if number % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")   
+
+
+## Movie tickets discount:
+
+# age = int(input("enter the age"))
+# if age < 12:
+#     print("ticket price: 100")
+# elif 12 <= age <= 60:
+#     print("ticket price: 200")
+# else:
+#     print("ticket price: 150") 
+
+# x = "10"
+# y = 7
+# z = int(x)
+# print(y + z)
+# a = 6
+# b = "21"
+# c = str(a)
+# print(b + c)
+
+# collage_name = input("enter collage name:")
+# if collage_name =="NSTIW":
+#     print("Welcome to NSTIW")
+# Trade = input("enter your trade:")
+# if Trade == "AIPA":  
+#     print("you are student of aritificial intelligence ")
+# elif Trade == "COPA":
+#     print("you are student of computer opretor programing assistant")
+# elif Trade == "DM":
+#     print("you are student of dress making")
+# else:
+#     print("you are not student")
+
+# State_name = input("enter your state name:")
+# language = input("enter your language:")
+# if  language == "Marathi":
+#     print("you are a maharashtriyan")
+# elif language == "Tamil":
+#     print("you are a tamilians")
+# elif language == "Bhojpuri":
+#     print("you are a bihari")
+# else:
+#     print("you are a other state")
+
+#Voting Eligible:
+# age =int(input ("enter your age:"))
+# citizenship = input("enter chitizenship:")
+# if age >= 18:
+#      if citizenship== "indian":
+#        print("eligible to vote in india")
+#      else:
+#       print("not eligible due to chitizenship")
+# else:
+#       print("not eligible due to age.")
+
+
+ #Even and Divisible:
+# number = int(input("enter a number:")) 
+# if number % 2==0:
+#      if number % 5 == 0:
+#       print("even and divisible by 5") 
+#      else:
+#        print ("even but not divisible by 5")
+# else:
+#    ("odd number.")
+
+
+#Student Grade:
+# marks = int(input("enter a number:"))
+# if marks >= 40:
+#     if marks>= 90:
+#         print("excellent")
+#     elif marks>= 75:
+#         print("very good")
+#     else:
+#         print("good")
+# else:
+#     print("fail.")
+
+
+#Temperature Check:
+# Temperature = int(input("enter the temperature:"))
+# if Temperature > 0:
+#    if Temperature < 20:
+#         print("it's cool")
+#    else:
+#         print("it's warm")
+# else:
+#  print("it's freezing.")
+
+
+# #Moving Ticket Discount:
+# age = int(input("enter the age:"))
+# day = input("enter the day(monday to sunday):")
+# if age < 12:
+#     if day == "sunday":
+#         print("free ticket for kids!")
+#     else:
+#         print("half price ticket")
+# else:
+#     print("full price ticket.")
+
+
+# Login System:
+username = input("enter the username:")
+password = input("enter the password:")
+if username == "motupatlu":
+    if password == "1234":
+        print("login successful")
+    else:
+        print("incorrect password")
+else:
+    print("invaild username.")
+
+#Online Shoppping Discount:
+# amount = int(input("enter purchase amount:"))
+# purchase = input("are u a member?(yes/no):")
+# if amount > 1000:
+#     if purchase == "yes":
+#         print("20% discount applied")
+#     else:
+#         print("10% discount applide")
+# else:
+#  print("no discount availible.")
+
+
+# # Exam Result Checker:
+# marks = int(input("enter marks(in%):"))
+# if marks >= 40:
+#     attendance = int(input("enter attendance(in%):"))
+#     if attendance >= 75:
+#         print("pass")
+#     else:
+#         print("pass but low attendance warning")
+# else:
+#     print("fail.")
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
