07ae26485bc34628aabf6fd49482b6b6d18637d0
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 15:00:47 +0530

    Add files via upload

---
+++ practice8.py
@@ -0,0 +1,20 @@
+# Integer values
+int1 = 10
+int2 = -5
+int3 = 0
+
+# Converting integers to floats
+float1 = float(int1)
+float2 = float(int2)
+float3 = float(int3)
+
+# Printing the results
+print("original integer values:")
+print("int1 =", int1)
+print("int2 =", int2)
+print("int3 =", int3)
+
+print("\nconverted float values:")
+print("float1 =", float1)
+print("float2 =", float2)
+print("float2 =", float3)
