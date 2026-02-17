c119622a00cadfc1b1793210eb4f837a230fb08c
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 15:03:02 +0530

    Add files via upload

---
+++ practice3.py
@@ -0,0 +1,74 @@
+#Python Program
+print("Hello, Python Learners!")
+name= 'Adit' 
+age= 18
+print(f"My name is {name} and i am {18} years old.")
+
+a=10
+b= 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+#print("addition, subtraction, multiplication, and division using these variables".)
+#Add two numbers
+p=6
+r=5
+print(p+r)
+print(
+    """
+    python is fun!
+    Let's learn it step by step
+    Keep practicing every day.
+    """)
+print("Python is fun! \nLet's learn it step by step. \nKeep practicing every day.")
+x="shivali" 
+y="singh"
+print("My name is " + x + y)
+
+x=5
+print(type(x))
+
+name= "shivali"
+print(type(name))
+
+y=5.6
+print(type(y))
+
+
+p= 8
+r= 4
+print(p>r)
+print(p<r)
+
+b= 9
+c= "shivali"
+z=str(b)
+print(c+z)
+
+s= 6
+k= "3"
+t= int(k)
+print(s+t)
+
+#x="10"
+#y=10
+#print( x + y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
