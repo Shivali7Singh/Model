672ac523edde2a870af9baeb96ea3a132b4f82b7
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 15:13:51 +0530

    Add files via upload

---
+++ lab12a.py
@@ -0,0 +1,52 @@
+# Assiging integer literals to variables
+integer1 = 10
+integer2 = -5
+integer3 = 0
+
+print("integer values:")
+print("integer1 =", integer1)
+print("integer2 =", integer2)
+print("integer3=", integer3)
+
+# Performing operations on integer literals
+print("sum of integer1 and integer2:", integer1 + integer2)
+print("product of integer1 and 3:", integer1 * 3)
+print("difference between integer1 and integer2:", integer1 - integer2)
+
+# Assigning floating-point literals to variables
+float1 = 3.14
+float2 = -2.718
+float3 = 0.0
+
+# Performing operations with floating-point numbers
+sum_floats = float1 + float2
+product_floats = float1 * 2
+difference_floats = float1 - float2 
+
+# Printing floating-point values and results of operations
+print("\nFloating-point valuse:")
+print("float1 =", float1)
+print("float2 =", float2)
+print("float3 =", float3)
+print("sum of float1 and float2:", sum_floats)
+print("product of float and 2:", product_floats)
+print("difference between float1 and float2:", difference_floats)
+
+# Assiging complex literals to variables
+complex1 = 2 + 3j
+complex2 = -1 - 4j
+complex3 = 0 + 0j
+
+# Performing operations with complex numbers
+sum_complex = complex1 + complex2
+product_complex = complex1 * (1 + 1j)
+diffrenece_complex = complex1 - complex2
+
+# Printing complex values and result of operations
+print("\ncomplex values:")
+print("complex2 =", complex1)
+print("complex2 =", complex2)
+print("complex3 =", complex3)
+print("sum of complex1 and complex2:", sum_complex)
+print("product of complex1 and (1 + 1j):", product_complex)
+print("difference between complex1 and complex2:", diffrenece_complex)
