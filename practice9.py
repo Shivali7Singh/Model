6d83d58bafe814b658788dd4480b35bd4a916086
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 15:00:16 +0530

    Add files via upload

---
+++ practice9.py
@@ -0,0 +1,50 @@
+# Defne a string and an integer
+string_value = "The number is "
+integer_value = 42
+
+# Convert the integer to a string using explicit conversion
+integer_as_string = str(integer_value)
+
+# Concatenate the string and the converted integer
+result = string_value + integer_as_string
+
+# Prin the result
+print(result)
+
+# Define two numbers
+a = 15
+b = 4
+
+# Addition
+addition_result = a + b
+print("addition:{} + {} = {}".format(a, b, addition_result))
+
+# Subtraction
+subtraction_result = a - b
+print("subtraction:{} - {} = {}".format(a, b, subtraction_result))
+
+# Multipication
+multification_result = a * b
+print("multification:{} * {} = {}".format(a, b, multification_result))
+
+# Division
+division_result = a / b
+print("division: {} / {} = {:.2f}".format(a, b, division_result))
+
+# Floor Division 
+floor_division_result = a // b
+print("floor division: {} // {} = {}".format(a, b, floor_division_result))
+
+# Modulus
+modulus_result = a % b
+print("modulus: {} % {} = {}".format(a, b, modulus_result))
+
+# Exponentiation
+exponentiation_result = a ** b
+print("exponentiation: {} ** {} = {}".format(a, b, exponentiation_result))
+
+
+
+
+
+
