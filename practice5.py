5b3e63da89a969c2f82adcc8e474c4291db39451
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 15:02:02 +0530

    Add files via upload

---
+++ practice5.py
@@ -0,0 +1,18 @@
+# s=input("enter your first name")
+# r=input("enter your last name")
+# q=s+r
+# print("enter your full name:",q)
+
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+z=x+y
+v=x-y
+q=x*y
+k=x/y
+print("The sum of two numbers is:",z)
+print("The diffrent of two numbers is:",v)
+print("The multification of two numbers:",q)
+print("The division of two numbers:",k)
+
+
+
