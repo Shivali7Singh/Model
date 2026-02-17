c6f28996291c01dc0cb39501894788f3524b48a8
Shivali7Singh <shivalisingh168@gmail.com>
2026-02-17 14:57:54 +0530

    Add files via upload

---
+++ self practice.py
@@ -0,0 +1,102 @@
+# # Airthmatic operator python:
+#  a = 12
+# b = 3
+# x = a + b
+# y = a - b
+# z = a * b
+# w = a / b
+# q = a ** b
+# s = a // b
+# print(x)
+# print(y)
+# print(z)
+# print(w)
+# print(q)
+# print(s)
+
+# Compartive operator:
+# p = 9
+# r = 6
+# print(p>r)
+# print(p<r)
+# print(p==r)
+# print(p>=r)
+# print(p<=r)
+# print(p!=r)
+
+# Logical oprator:
+# q = 6
+# t = 7
+# u = 8
+# print(q<t and t<u)
+# print(t<u or q>t)
+# print(not t>q)
+
+# Assign oprator:
+# x = 9
+# y = 3
+# x +=y
+# print(x)
+
+# x -=y
+# print(x)
+
+# x *=y
+# print(x)
+
+# x /=y
+# print(x)
+
+# x **=y
+# print(x)
+
+# x //=y
+# print(x)
+
+# x %=y
+# print(x)
+
+# x !=y
+# print(x)
+
+# a = 6
+# b = 12
+# a**=b
+# print(a)
+
+# a//=b
+# print(a)
+
+# a%=b
+# print(a)
+ 
+balance = 100
+withdrow = int(input("Enter amount to withdrow:"))
+
+if withdrow <= balance:
+    print("Transaction successfull!")
+
+else:
+    print("Garibi!")
+
+
+# vehical =input("Enter your vehical name:")
+
+# if vehical in ("BMW", "creta", "maruti swift", "hyundai"):
+#     print("it is a four wheelers")
+
+# elif vehical in ("honda", "bajaj", "apache"):
+#      print ("it is a two wheelers")
+
+# else:
+#      print("unkown vehical")
+
+
+
+
+
+
+
+
+
+
